7829a69e1dc093b6ff28868f272b8935123d491e
pjyoung0 <junyoung37590@yonsei.ac.kr>
2026-06-11 19:05:38 +0900

    Update us_market.py

--- us_market.py
+++ us_market.py
@@ -1,9 +1,27 @@
 import yfinance as yf
 
-indices = {
-
-    "S&P500":"^GSPC",
-    "NASDAQ":"^IXIC",
-    "Russell2000":"^RUT"
-
+INDICES = {
+    "S&P500": "^GSPC",
+    "NASDAQ": "^IXIC",
+    "Russell2000": "^RUT"
 }
+
+def get_market_data():
+
+    result = {}
+
+    for name, ticker in INDICES.items():
+
+        hist = yf.Ticker(ticker).history(period="5d")
+
+        close = hist["Close"]
+
+        pct = (
+            close.iloc[-1]
+            / close.iloc[-2]
+            - 1
+        ) * 100
+
+        result[name] = round(pct, 2)
+
+    return result
