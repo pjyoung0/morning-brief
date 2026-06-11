7d97dc5542e0c824a0a08fc34435df8ca9b71cbb
pjyoung0 <junyoung37590@yonsei.ac.kr>
2026-06-11 19:16:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
 from us_market import get_market_data
-from ai_sector import get_sector_data
+
+from ai_sector import (
+    get_sector_data,
+    get_korea_watchlist
+)
+
+from dart_disclosure import (
+    get_dart_disclosure
+)
+
+from gemini_report import (
+    generate_report
+)
+
+from email_sender import (
+    send_email
+)
 
 market = get_market_data()
+
 sector = get_sector_data()
 
-print(market)
-print(sector)
+watchlist = get_korea_watchlist()
+
+dart = get_dart_disclosure()
+
+report = generate_report(
+
+    market,
+    sector,
+    watchlist,
+    dart
+
+)
+
+print(report)
+
+send_email(report)
