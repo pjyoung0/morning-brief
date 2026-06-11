88b71d0a33423dd31afe979c51cfc400b85d6121
pjyoung0 <junyoung37590@yonsei.ac.kr>
2026-06-11 19:14:56 +0900

    Update ai_sector.py

--- ai_sector.py
+++ ai_sector.py
@@ -47,3 +47,51 @@ def get_sector_data():
         result[sector] = detail
 
     return result
+
+KOREA_MAPPING = {
+
+    "반도체":[
+        "SK하이닉스",
+        "한미반도체",
+        "브이엠",
+        "테크윙",
+        "ISC"
+    ],
+
+    "광통신":[
+        "오이솔루션",
+        "성호전",
+        "대한광통신"
+    ],
+
+    "전력기기":[
+        "HD현대일렉트릭",
+        "LS ELECTRIC",
+        "효성중공업",
+        "산일전기"
+    ]
+}
+
+def get_korea_watchlist():
+
+    sector_data = get_sector_data()
+
+    watchlist = {}
+
+    for sector, detail in sector_data.items():
+
+        avg_return = (
+            sum(detail.values())
+            / len(detail)
+        )
+
+        if avg_return > 0:
+
+            watchlist[sector] = (
+                KOREA_MAPPING.get(
+                    sector,
+                    []
+                )
+            )
+
+    return watchlist
