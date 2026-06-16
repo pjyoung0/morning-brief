01c520201efe47878ed0ba442741ed57eabeabab
pjyoung0 <junyoung37590@yonsei.ac.kr>
2026-06-16 22:17:41 +0900

    Create news_tracker.py

---
+++ news_tracker.py
@@ -0,0 +1,28 @@
+import feedparser
+
+NEWS_KEYWORDS = [
+
+    "NVIDIA",
+
+    "Broadcom",
+
+    "Marvell",
+
+    "TSMC",
+
+    "ASML",
+
+    "Vertiv",
+
+    "Eaton",
+
+    "Coherent",
+
+    "Tesla"
+]
+
+https://feeds.finance.yahoo.com/rss/2.0/headline
+
+def get_ai_news():
+
+  
